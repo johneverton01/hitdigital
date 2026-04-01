b112395132b59cfaa27cb6a96e0cbc2bceedaa4b
John Everton <johneverton44@gmail.com>
2026-04-01 09:42:01 -0300

    feat: create default layout

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex h-screen items-center justify-center"dd>
-    <h1 class="text-2xl font-bold">Hello, World!</h1>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-slate-900 text-white h-screen">
+    <div class="w-300 mx-auto py-2 px-2 space-y-4">
+      <header>menu</header>
+      <main>
+        <slot />
+      </main>
+      <footer>
+        <p class="text-sm text-gray-400">Copyright 2026</p>
+      </footer>
+    </div>
+  </div>
+</template>
